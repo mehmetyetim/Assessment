ef3f86c2cfa57efa9810cda03a7b99c941fe15cd
mehmetyetim <98969298+mehmetyetim@users.noreply.github.com>
2022-05-09 07:10:44 +0100

    task_3 second version added

--- task_3.py
+++ task_3.py
@@ -1,9 +1,12 @@
 import sys
 
+import pandas as pd
 
-# user needs to load file after each time program gives the option of load
-def layout():
+split_list = []
+
+if __name__ == '__main__':
     i = 0
+    df = pd.DataFrame()
     print("Log File Analyser" + "\n=================\n")
     print("L: Load / Reload File")
     print("S: Show Basic Statistics")
@@ -14,39 +17,28 @@ def layout():
         if i == 0 and (operation == 'S' or operation == 'H'):
             sys.exit("No log file loaded. Do that first.")
         if operation == 'L':
-            i+=1
-            with open("access.log", "r") as f:
-                lines = f.read()
-            attempts = lines.split("\n")
-            status_codes_list = []
-            for i in range(len(attempts)):
-                uri = attempts[i].split(',')[1]
-                status_code = attempts[i].split(',')[2]
-                status_codes_list.append(status_code)
-        elif operation == 'S':
-            print('Total Entries:',len(attempts))
-            break;
+            i += 1
+            exec(open('log_file_generator.py').read())
+            log_data = open('*.log', 'r')
+            for line in log_data:
+                ip = line.split(',')[0]
+                uri = line.split(',')[1]
+                status_code = int(line.split(',')[2][:3])
+                split_list.append([ip, uri, status_code])
+            df = pd.DataFrame(split_list, columns=['ip', 'uri', 'status_code'])
         elif operation == 'H':
             print("HTTP Code Summary\n=================\n")
-            print("200 : \t" + str(status_codes_list.count("200")) +
-                  "(" + str(status_codes_list.count("200") / len(status_codes_list)) + "%)")
-            print("201 : \t" + str(status_codes_list.count("201")) +
-                  "(" + str(status_codes_list.count("201") / len(status_codes_list)) + "%)")
-            print("400 : \t" + str(status_codes_list.count("400")) +
-                  "(" + str(status_codes_list.count("400") / len(status_codes_list)) + "%)")
-            print("401 : \t" + str(status_codes_list.count("401")) +
-                  "(" + str(status_codes_list.count("401") / len(status_codes_list)) + "%)")
-            print("404 : \t" + str(status_codes_list.count("404")) +
-                  "(" + str(status_codes_list.count("404") / len(status_codes_list)) + "%)")
-            print("500 : \t" + str(status_codes_list.count("500")) +
-                  "(" + str(status_codes_list.count("500") / len(status_codes_list)) + "%)")
-            layout()
+            for i in range(0, len(df['status_code'].unique())):
+                print(str(df['status_code'].sort_values().unique()[i]) + ":\t" + str(
+                    len(df.loc[df['status_code'] == df['status_code'].sort_values().unique()[i]]))
+                      + " (" + str('%.2f' % float(
+                    len(df.loc[df['status_code'] == df['status_code'].sort_values().unique()[i]]) * 100 / len(
+                        df))) + "%)")
+
+        elif operation == 'S':
+            print('Total Entries:', len(df.index))
+            break
         elif operation == 'Q':
             sys.exit()
         else:
             raise Exception("The operation you entered is not listed.")
-
-
-
-if __name__ == '__main__':
-    layout()
